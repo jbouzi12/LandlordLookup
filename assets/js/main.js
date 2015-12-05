e0cf26dea5f10d21799620a98bedc8595b374e45
Jensen Bouzi <jensen.bouzi@gmail.com>
2015-12-05 15:43:07 -0500

    can now get bbl & address data

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,11 +36,11 @@ function getTaxData(data) {
     });
   }
 
-function parseGoogle(place) {
+function parseGoogle(place, borough) {
     console.log("in parseGoogle");
     var $dfd = new $.Deferred();
 
-    console.log(place.address_components);
+    // console.log(place.address_components);
 
     if (!place.address_components) {
       return $dfd.reject("Sorry, I can't work with that address");
@@ -51,8 +51,7 @@ function parseGoogle(place) {
     // }
     
     var houseNumber = place.address_components[0].short_name,
-        street = place.address_components[1].short_name,
-        borough;
+        street = place.address_components[1].short_name;
 
     // for (var i = 0; i < place.address_components.length; i += 1) {
     //   var county = place.address_components[i].long_name;
@@ -63,8 +62,6 @@ function parseGoogle(place) {
     //     break;
     //   }
     // }
-
-    var borough = $()
 
     if (!borough) {
       return $dfd.reject("Unknown borough");
@@ -85,26 +82,33 @@ function parseGoogle(place) {
 
 $(document).ready(function () {
 
+    var place;
+
     $('#lookup-form').on('submit', function (evt) {
       evt.preventDefault();
+      var borough = $('#sel1').val();
+
+      parseGoogle(place, borough)
+        .then(getBBL)
+        .then(function(data) {
+          console.log('tax data', data);
+        })
+        .fail(function(){
+          console.log('something failed');
+        });
+
     });
 
     var autocomplete = new google.maps.places.Autocomplete($('#address_lookup')[0]);
     autocomplete.setTypes(['address']);
-    console.log(autocomplete);
+    // console.log(autocomplete);
     autocomplete.setBounds(new google.maps.LatLngBounds(
       new google.maps.LatLng(40.49,-74.27),
       new google.maps.LatLng(40.87,-73.68)
     ));
+    
     autocomplete.addListener('place_changed', function() {
-      var place = autocomplete.getPlace();
-      parseGoogle(place)
-        .then(getBBL)
-        .then(getTaxData)
-        .then(function (data) {
-          console.log(data);
-        })
-        .fail(showError);
+      place = autocomplete.getPlace();
     });
 
 
